9aab7903ba83953ef6e43c21f4ef842c3df6f459
erri120 <erri120@protonmail.com>
2021-02-08 09:53:51 +0100

    Update Ccache.cmake to 1.2.2

--- cmake/Tools.cmake
+++ cmake/Tools.cmake
@@ -2,6 +2,6 @@ if (USE_CCACHE)
     CPMAddPackage (
             NAME Ccache.cmake
             GITHUB_REPOSITORY TheLartians/Ccache.cmake
-            VERSION 1.2
+            VERSION 1.2.2
     )
 endif()
